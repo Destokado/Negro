01d33e697960c9ee7bc7b5d5cb7a028bb47ca694
guplem <guplem@gmail.com>
2020-02-20 14:04:15 +0100

    Fade between events

---
+++ Negro/Assets/Scripts/ImageOpacityAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageOpacityAnimation : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve animationCurve;
+    private Image image;
+    private float objectiveVal;
+    private float originalVal;
+    private float animationDuration;
+    private float currentAnimationElapsedTime;
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void SetOpacityTo(float value, float time)
+    {
+        originalVal = image.color.a;
+        objectiveVal = value;
+        animationDuration = time;
+        currentAnimationElapsedTime = 0f;
+    }
+
+    private void Update()
+    {
+        image.raycastTarget = Math.Abs(image.color.a) > 0.05f;
+        
+        if (Math.Abs(objectiveVal - image.color.a) < 0.001f)
+            return;
+        
+        Color currentColor = image.color;
+        image.color = new Color(currentColor.r, currentColor.g, currentColor.b, GetValueTroughAnimCurve(originalVal, objectiveVal, currentAnimationElapsedTime, animationDuration, animationCurve));
+        
+        currentAnimationElapsedTime += Time.deltaTime;
+    }
+
+    public static float GetValueTroughAnimCurve(float origin, float objective, float timeElapsed, float animationDuration, AnimationCurve animationCurve)
+    {
+        float progress = animationDuration > 0 ? animationCurve.Evaluate(timeElapsed / animationDuration) : 1;
+        return Mathf.Lerp(origin, objective, progress);
+    }
+}
--- Negro/Assets/Scripts/UIManager.cs
+++ Negro/Assets/Scripts/UIManager.cs
@@ -8,23 +8,41 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     private Action chosenAction;
+    [Range(0f, 5f)]
+    [SerializeField] private float fadeDuration = 1f;
     [SerializeField] private  ActionButton [] actionButtons;
-    
     [SerializeField] private Image eventBackground;
     [SerializeField] private TextMeshProUGUI eventText;
+    [SerializeField] private ImageOpacityAnimation blackPanel;
 
+    private void Start()
+    {
+        blackPanel.SetOpacityTo(1f, 0); // Fade to full opaque
+    }
+
+    private Event currentEvent = null;
     public void DrawEvent(Event ev)
+    {
+        currentEvent = ev;
+        blackPanel.SetOpacityTo(1f, fadeDuration); // Fade to full opaque
+        Invoke(nameof(DrawCurrentEvent), fadeDuration); // Change the event after the fade to full opaque ends
+    }
+    
+    private void DrawCurrentEvent()
     {
         for (int i = 0; i < actionButtons.Length; i++)
         {
-            actionButtons[i].gameObject.SetActive(ev.actions[i].CanActionBeShownInGame());
-            actionButtons[i].SetUp(ev.actions[i]);
+            actionButtons[i].gameObject.SetActive(currentEvent.actions[i].CanActionBeShownInGame());
+            actionButtons[i].SetUp(currentEvent.actions[i]);
         }
         
-        eventBackground.sprite = Resources.Load<Sprite>(ev.art);
-        eventText.text = ev.text;
+        eventBackground.sprite = Resources.Load<Sprite>(currentEvent.art);
+        eventText.text = currentEvent.text;
+        
+        
+        blackPanel.SetOpacityTo(0f, 1f); // Fade to non opacity
     }
-
+    
     public void ShowConsequencesOf(Stats statsModification, Stats resultGameStats)
     {
         // TODO
