d749ef0729112989a1ae96955e99180e25718722
guplem <guplem@gmail.com>
2020-03-04 17:49:03 +0100

    Improved aesthetics

--- Negro/Assets/Scripts/UIManager.cs
+++ Negro/Assets/Scripts/UIManager.cs
@@ -14,15 +14,10 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image eventBackground;
     [SerializeField] private TextMeshProUGUI eventText;
     
-    [SerializeField] private  ActionButton consequenceAction;
-    [SerializeField] private Image consequenceBackgroundAndRoot;
-    [SerializeField] private TextMeshProUGUI consequenceText;
-    
     [SerializeField] private ImageOpacityAnimation blackPanel;
     
     [SerializeField] private VideoController videoController;
-
-    //private Event currentEvent = null;
+    
     [SerializeField] private int thresholdToShowEffectVideos;
 
     [SerializeField] private PauseMenu pauseMenu;
@@ -30,25 +25,14 @@ public class UIManager : MonoBehaviour
     
     public void DrawEvent(Event ev)
     {
-        if (ev.validActions == 1)
+        for (int i = 0; i < actionButtons.Length; i++)
         {
-            consequenceBackgroundAndRoot.gameObject.SetActive(true);
-            consequenceAction.SetUp(ev.GetFirstValidAction());
-            consequenceBackgroundAndRoot.sprite = Resources.Load<Sprite>(ev.art);
-            consequenceText.text = ev.text.EnsureEndsWithDot();
-        }
-        else
-        {
-            consequenceBackgroundAndRoot.gameObject.SetActive(false);
-            for (int i = 0; i < actionButtons.Length; i++)
-            {
-                actionButtons[i].gameObject.SetActive(ev.actions[i].IsValid());
-                actionButtons[i].SetUp(ev.actions[i]);
-            }
-            eventBackground.sprite = Resources.Load<Sprite>(ev.art);
-            eventText.text = ev.text.EnsureEndsWithDot();
+            actionButtons[i].gameObject.SetActive(ev.actions[i].IsValid());
+            actionButtons[i].SetUp(ev.actions[i]);
         }
         
+        eventBackground.sprite = Resources.Load<Sprite>(ev.art);
+
         SetBlackScreenTo(false); // Change the event after the fade to full opaque ends
     }    
 
