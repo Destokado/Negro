f225e4670b4c57c1347aff51f2ce09745f0f4a74
gillemp <guplem@gmail.com>
2020-03-05 14:02:09 +0100

    Music

---
+++ Negro/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[] music;
+    private int currentMusicIndex = -1;
+    private AudioSource audioSource;
+
+    private static MusicManager instance;
+    
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayAllSongs();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayAllSongs();
+        }
+    }
+
+    private void PlayAllSongs()
+    {
+        CancelInvoke(nameof(PlayAllSongs));
+        
+        currentMusicIndex++;
+        if (currentMusicIndex > music.Length - 1)
+            currentMusicIndex = 0;
+        
+        audioSource.clip = music[currentMusicIndex];
+        audioSource.Play();
+        
+        Invoke(nameof(PlayAllSongs), audioSource.clip.length);
+    }
+}
